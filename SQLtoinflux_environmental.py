1d002232ec9e93536d0c0528760eec981ea8c45b
Torounia <3080723+Torounia@users.noreply.github.com>
2020-11-08 19:26:40 +0000

    First commit

---
+++ SQLtoinflux_environmental.py
@@ -0,0 +1,113 @@
+import pypyodbc 
+import pandas as pd
+from datetime import datetime, timedelta
+import time
+import argparse
+from influxdb import InfluxDBClient
+
+cnxn = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                        "Server=LLSANDBOX-PC\SQLEXPRESS;"
+                        "Database=HBT.ECD.Awair;"
+                        "uid=SQLadmin;pwd=LL22@@")
+
+cursor = cnxn.cursor()
+cursor.execute("SELECT * FROM [HBT.ECD.Awair].[dbo].[HBT18019000_Data] where [DPTimeStamp] >" + ((datetime.now() - timedelta(hours=25)).strftime('%y%m%d%H%M%S')) + " order by [DPTimeStamp] desc")
+rows = cursor.fetchall()
+
+client = InfluxDBClient('localhost', 8086, 'root', 'root', 'building_systems')
+
+
+
+
+def convert_timestamps(timestamp):
+    return int(time.mktime(datetime.strptime(timestamp, '%y%m%d%H%M%S').timetuple())) 
+
+
+
+'''
+DeviceAddress	        DeviceName	DeviceDescription	DeviceState	    DeviceTimeStamp	DeviceType	           DPAddress	DPName	      DPDescription	   DPState	DPTimeStamp	DPType	DPValue	Comments
+154	                    Trend_BMS	london_first_mtgr4		            200827220655	BMS_sensor  	        0	        Space R105	   DegC		                200827220655	AI	24.435	
+
+DeviceAddress	        DeviceName	DeviceDescription	 DeviceState    DeviceTimeStamp	DeviceType	             DPAddress	DPName	       DPDescription	DPState	DPTimeStamp	DPType	DPValue	Comments
+Qnc8LeIoRkC8DqiM3nF-Mg	1_REC1	    london_first_general	            200827210421	occupancy_sensor		             1_REC1	        TRAFFIC		            200827210421		0,0	    200827210421
+uZ69pY5FQduC9A_KLqg9yA	MTG2	    london_first_mtgr2		            200827210451	occupancy_sensor		              MTG2	        COUNTING	    0	    200827210451		,	    200827210451
+
+DeviceAddress	        DeviceName	DeviceDescription	DeviceState	    DeviceTimeStamp	DeviceType	            DPAddress	DPName	        DPDescription	DPState DPTimeStamp	DPType	DPValue	Comments
+993	                    OMNI 001 - 	london_first_general_4A		        200827220502	environmental_sensor	temp	    OMNI 001 - WTS	temp		            200827220502	temp	24.9899997711182	
+993	                    OMNI 001 - 	london_first_general_4A		        200827220502	environmental_sensor	pm25	    OMNI 001 - WTSy	pm25		            200827220502	pm25	2	
+
+
+Device Name
+Omni…
+1_rec1
+Trend_BMS
+
+Device Description (location)
+
+
+Device_Type = DeviceType
+
+Datapoint_name
+Environmental = DPDescrption
+Occupancy = DPDescrption
+BMS = DPname
+
+'''
+
+device_name = [] #DeviceName
+device_location = [] #DeviceDescrption yes all
+device_type = [] #DeviceType yes all
+datapoint_name = [] #DPname 
+datapoint_time = [] #dptimestamp yes all
+datapoint_value = [] #dpvalue datapoint value for the rest
+for row in rows:
+    device_name.append(row[1])
+    device_location.append(row[2])
+    device_type.append(row[5])
+    datapoint_name.append(row[8])
+    datapoint_time.append(convert_timestamps(row[10]))
+    datapoint_value.append(row[12])
+
+
+
+influx_points = []
+
+for row in range(len(datapoint_time)):
+    influx_points.append(
+        {
+        "measurement": "Envirnmental",
+        "tags": {
+            "device_name": device_name[row],
+            "device_location_office": device_location[row].split('_')[0],
+            "device_location_floor": device_location[row].split('_')[1],
+            "device_location_area": device_location[row].split('_')[2],
+            "device_type": device_type[row],
+            "datapoint-name": datapoint_name[row]
+
+        },
+        "time": datapoint_time[row],
+        "fields": {
+            "value": float(datapoint_value[row]),
+            }
+    })
+    
+
+client_write_start_time = time.perf_counter()
+
+client.write_points(influx_points, database='building_systems', time_precision='s', batch_size=10000)
+
+client_write_end_time = time.perf_counter()
+
+print('write complete')
+
+print("Client Library Write: {time}s".format(time=client_write_end_time - client_write_start_time))
+
+
+#TODO
+'''
+Done - rename measurements to occupancy and environmental from building systems
+create logging system
+done - create scheduler every 24 hours
+find a way to export to CSV
+
+'''
